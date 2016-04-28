f6672ffb10b1329dfa80e054199e7dd9ecbe80ca
Marlysson Silva <marlysson5@gmail.com>
2016-04-28 00:17:09 -0300

    Resolvendo problema diferença absolute das diagonais principal e secundária de uma matriz

---
+++ Algorithms/Warmup/diagonal_diference.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+arquivo = open("matriz.txt")
+matriz = []
+
+for linha in arquivo:
+	vetor = linha.strip().split(" ")
+	matriz.append( map(int,vetor) )
+
+def get_princ(matriz):
+
+	cont = 0
+	sum_princ = 0
+
+	for vetor in matriz:
+		sum_princ += vetor[cont]
+		cont += 1
+
+	return sum_princ
+
+
+def get_sec(matriz):
+	pos = len(matriz) - 1
+	index_vetor = 0
+	sum_sec = 0
+
+	while index_vetor < len(matriz):
+		sum_sec += matriz[index_vetor][pos]
+	
+		pos -= 1
+		index_vetor += 1
+
+	return sum_sec
+
+print("Soma diagonal principal:  {}".format(get_princ(matriz)))
+print("Soma diagonal Secundária: {}".format(get_sec(matriz)))
+
+resultado = abs( get_princ(matriz) - get_sec(matriz))
+
+print ("Diferença absoluta: {}".format(resultado))
